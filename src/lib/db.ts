76ae9184d77d738f7f43da670598aa633c45e1d6
LLsmile9 <LLsmile9@users.noreply.github.com>
2026-05-23 14:25:33 +0000

    修复: PrismaLibSQL大小写修正

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { PrismaLibSql } from '@prisma/adapter-libsql'
 import { createClient } from '@libsql/client'
 
 const globalForPrisma = globalThis as unknown as {
@@ -15,7 +15,7 @@ function createPrismaClient() {
       url,
       authToken: process.env.DATABASE_AUTH_TOKEN || '',
     })
-    const adapter = new PrismaLibSQL(libsql)
+    const adapter = new PrismaLibSql(libsql)
     return new PrismaClient({ adapter })
   }
 
